0b3b6dbcfaba0eeef1b8e5a975110bb312f82d01
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-01-01 18:20:19 -0300

    Add project files.

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,13 @@
+﻿/* 3 - Escreva um program onde o usuário é solicitado a informar um valor via teclado e armazenar o valor na 
+    variável entrada do tipo string onde tem que verificar 3 condições diferentes:
+
+    a- Se a entrada é nula, uma exceção ArgumentNullException é lançada com a mensagem "A entrada não pode ser 
+    nula." (Verifique a diferença entre ArgumentException e ArgumentNullException)
+
+    b-Se a entrada está vazia, uma exceção ArgumentException é lançada com a mensagem "A entrada não pode estar 
+    vazia."
+
+    c-Se a entrada passar nas verificações anteriores, a entrada é exibida na tela.
+    Realize o tratamento de erro para essas condições usando o bloco try-cath-finally
+ 
+ */
