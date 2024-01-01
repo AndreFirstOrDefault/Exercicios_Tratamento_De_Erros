2f09530840b445cf0b5e87e2948806f39db944bf
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-01-01 20:57:52 -0300

    Exercicio 05 finalizado

---
+++ Exercicio05/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ 5- Dado um array de inteiros expresso da seguinte forma
+    int[] numeros = new int[] { 109, 211, 313, 405, 519, 617, 711, 891, 951, 1001 };
+    Exiba na janela do console os numeros do array e solicite via teclado ao usuário para informar o valor de um índice 
+    do array para obter o seu respectivo valor.
+
+    Realize o tratamento de exceções filtrando as exceções IndexOutOfRangeException e ArgumentNullException
+ */
+
+int[] numeros = new int[] { 109, 211, 313, 405, 519, 617, 711, 891, 951, 1001 };
+
+// Exibindo numeros do array
+
+for(int i = 0; i < numeros.Length; i++)
+{
+    Console.Write(numeros[i] + ", ");
+    if(i == numeros.Length - 1)
+    {
+        Console.WriteLine(numeros[i] + ".");
+    }
+}
+
+
+try
+{
+    Console.Write("\nDigite o número do indice que gostaria de consultar: ");
+    int indice = int.Parse(Console.ReadLine());
+    
+    Console.WriteLine($"\nNúmero corresponde ao indice: {numeros[indice]}\n");
+    
+}
+catch (IndexOutOfRangeException e)
+{
+    Console.WriteLine($"\nOcorreu uma excessão: {e.GetType()}, {e.Message}");
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine($"\nOcorreu uma excessão: {e.GetType()}, {e.Message}");
+}
+catch (Exception e)
+{
+    Console.WriteLine($"\nOcorreu uma excessão: {e.GetType()}, {e.Message}");
+}
+
+
+Console.ReadKey();
